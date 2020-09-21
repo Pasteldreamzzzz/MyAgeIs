8d594fe6edb9c66d1cd8c7b48a54b875b957c623
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2020-09-21 14:20:22 +0000

    Initialized and declared other variables

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
   public static void main(String[] args) {
     int myAge = 15; 
+    int myBirthday = 527; 
+    int todaysDate = 921; 
     
     System.out.println("I am " + myAge + " years old.");
   }
